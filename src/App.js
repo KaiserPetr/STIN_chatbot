9692aef5532ff7dbf0b7cbe148af69eb8521d499
Petr Kaiser <petr.kaiser@tul.cz>
2022-03-09 17:30:01 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from 'react-native';
+
+const ChatBot = () => {
+  return (
+    <View>
+      <Text>ChatBot</Text>
+    </View>
+  )
+}
+
+export default ChatBot;
